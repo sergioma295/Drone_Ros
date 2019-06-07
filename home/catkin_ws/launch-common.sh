1107bef06f47347dfb9aadebc3c7107f12a44bfd
sergioma295 <sergioma295@gmail.com>
2019-06-07 11:27:12 +0200

    Final configuration ROS, workspace and PX4/Firmware

---
+++ home/catkin_ws/launch-common.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+px4_dir='/home/sergio/src/Firmware'
+
+source $px4_dir/Tools/setup_gazebo.bash $px4_dir $px4_dir/build/posix_sitl_default
+
+source devel/setup.bash
+
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$px4_dir
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$px4_dir/Tools/sitl_gazebo
